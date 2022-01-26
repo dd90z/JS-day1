f9b1111616222b1ece59c407f4cd979759a0995d
Dean Delic <deandelic6@gmail.com>
2022-01-26 16:11:24 +0100

    js ad1

---
+++ exad1.js
@@ -0,0 +1,25 @@
+var a = 7;
+
+var b = 14;
+
+var c = "21";
+
+var d = '36';
+
+var e = 42;
+
+var sum = a + b +
+    +Number(c) + Number(d) + e;
+console.log(sum);
+
+var f = '1';
+
+var g = 15;
+
+var h = 8;
+
+var i = "1";
+
+var mt = Number(f) * g * h * Number(i);
+console.log(mt);
+document.write(sum / mt);
