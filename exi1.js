2e9cc64e7258fccfa86484f56fd3d37205574fa7
Dean Delic <deandelic6@gmail.com>
2022-01-26 16:11:44 +0100

    exi1

---
+++ exi1.js
@@ -0,0 +1,22 @@
+var auto = ["Tesla", "Audi", "Renault", "Volvo", "Mazda", "Fiat", "Ferrari"];
+console.log(auto.sort());
+
+let fruits = ["apple", "banana", "kiwi"];
+let animals = ["monkey", "horse", "dog"];
+console.log(fruits.sort());
+fruits.splice(3, 0, "orange")
+console.log(fruits.sort());
+
+console.log(animals.sort());
+animals.splice(0, 0, "cat")
+console.log(animals.sort());
+
+var trfruits = ["mango", "cherries", "kiwi", "grapes", "pear", "peach", "orange", "lemon"];
+console.log(trfruits[0]);
+console.log(trfruits[1]);
+console.log(trfruits[2]);
+console.log(trfruits[3]);
+console.log(trfruits[4]);
+console.log(trfruits[5]);
+console.log(trfruits[6]);
+console.log(trfruits[7]);
