d2ac0661f7968a3db35eda9f4550924c2bab307c
Dean Delic <deandelic6@gmail.com>
2022-01-26 14:25:45 +0100

    js day1

---
+++ exjs1.js
@@ -0,0 +1,8 @@
+// console.log("Hello World");
+// console.log("Hi my name is Martin");
+var players = ["Martin", "Thomas", "Peter", "Mathias", "Niki"];
+console.log(players[2]);
+console.log("The most valuable player of the match");
+
+// var team = ['Bill', 'Mary', 'Mike', 'Chris', 'Anne'];
+// console.log(team[2]);
